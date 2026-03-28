eaaa86cb5390e477cc78f640f6d2b6e987266600
Arjunsuresh1320 <arjunsuresh1320@gmail.com>
2026-03-28 21:19:33 +0530

    added tests

--- src/vitest.config.js
+++ src/vitest.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "jsdom",   // ✅ THIS FIXES YOUR ERROR
-    globals: true           // optional but recommended
+    environment: "jsdom",
+    globals: true
   },
 });
