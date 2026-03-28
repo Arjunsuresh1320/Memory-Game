dd64afe3e008b18d78077d737f5a07192f91dd47
Arjunsuresh1320 <arjunsuresh1320@gmail.com>
2026-03-28 21:32:25 +0530

    added tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,4 @@
-import { test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import App from "./App";
 
 test("renders game title", () => {
