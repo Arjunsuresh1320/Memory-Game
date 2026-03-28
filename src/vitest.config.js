4e3d353ea01ca127c648f73b3e4456f6a063a50a
Arjunsuresh1320 <arjunsuresh1320@gmail.com>
2026-03-28 21:29:05 +0530

    added tests

--- src/vitest.config.js
+++ src/vitest.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    globals: true
+    globals: true,
+    setupFiles: "./src/setupTests.js"
   }
 });
